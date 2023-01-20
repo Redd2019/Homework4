410e19aa143396bc0db5e828c3a2e34a27492d28
Redd2019 <anton-mikerin@yandex.ru>
2023-01-20 18:08:40 +0300

    Завершено Задание 4

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
     }
 
     public static void task1 () {
@@ -35,6 +36,25 @@ public class Main {
         }
         if (speed <= 60) {
             System.out.println("Если скорость " + speed + " км/ч, то можно ездить спокойно.");
+        }
+    }
+
+    public static void task4 () {
+        System.out.println("Задача 4");
+        int age = 21;
+        if (age >=2 && age <=6) {
+            System.out.println("Если возраст человека равен " + age + ",то ему нужно ходить в детский сад.");
+        }
+        if (age >=7 && age <=18) {
+            System.out.println("Если возраст человека равен " + age + ",то ему нужно ходить в школу.");
+        }
+        if (age >18 && age <=24) {
+            System.out.println("Если возраст человека равен " + age + ",то его место в университете.");
+        } else {
+            System.out.println("Если возраст человека равен " + age + ",то ему не место в университете.");
+        }
+        if (age > 24) {
+            System.out.println("Если возраст человека равен " + age + ",то ему пора на работу.");
         }
     }
 
