794367f736f994c238d09fa64ea4c89304cc2ded
Redd2019 <anton-mikerin@yandex.ru>
2023-01-20 16:55:33 +0300

    Завершено Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
+        task1();
+        task2();
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
         int age = 17;
         if (age >= 18) {
             System.out.println("Если возраст человека равен " + age + " , то он совершеннолетний.");
@@ -9,5 +15,15 @@ public class Main {
         }
     }
 
+    public static void task2 () {
+        System.out.println("Задача 2");
+        int temperature = 2;
+        if (temperature >= 5) {
+            System.out.println("Сегодня тепло, можно идти без шапки.");
+        }
+        if (temperature < 5) {
+            System.out.println("На улице холодно, нужно надеть шапку.");
+        }
+    }
 
 }
